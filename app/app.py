6f1803c6c59e6a0aede75504614315a69a18cdc2
Dencgik <dencgik@gmail.com>
2015-10-10 12:27:54 +0300

    Deleted comment string

--- app/app.py
+++ app/app.py
@@ -15,29 +15,12 @@ def show_entries():
 
 @app.route('/cities/')
 def get_sities():
-    # entries = mongo.db.cities.find().sort("pop", -1).limit(20)
     entries = mongo.db.cities.aggregate([{"$group": {"_id": "$city", "population": {"$sum":"$pop"}}},
     			{"$sort": {"population": -1}}, {"$limit": 20}])
     data = json.dumps(entries['result'])
     resp = Response(response=data, status=200, mimetype="application/json")
     # print data
     return (resp)
-
-
-# @app.route('/checkout/', methods=['GET', 'POST'])
-# def checkout():
-#     name = request.form.get('name')
-#     phone = request.form.get('phone')
-#     add_text = request.form.get('add_text')
-#     email = request.form.get('email')
-#     voss = request.form.getlist('voss')
-#     inf = {"name": name, "phone": phone, "add_text": add_text, "voss": voss, "email": email}
-#     list_voss = ', '.join(inf['voss'])
-#     text = render_template("email.txt", inf=inf, list_voss=list_voss)
-#     # text = 'dwawda'
-#     send_email('VOSS', ADMINS[0], ADMINS, text)
-#     return text
-
 
 if __name__ == "__main__":
     app.run(debug=True)
